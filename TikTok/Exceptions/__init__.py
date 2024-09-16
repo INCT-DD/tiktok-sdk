4f052ce6bf846bd264cfca3f773efde404fdbb8c
Alexandre Teles <1757773+alexandreteles@users.noreply.github.com>
2024-09-16 01:36:02 -0300

    docs: update namespace documentation

---
+++ TikTok/Exceptions/__init__.py
@@ -0,0 +1,5 @@
+"""
+This module serves as a namespace for the various exceptions used in the TikTok library.
+It encapsulates the functionality for handling errors and exceptional cases that may arise during
+interactions with the TikTok API, ensuring robust error management throughout the application.
+"""
---
+++ TikTok/Queries/__init__.py
@@ -0,0 +1,4 @@
+"""
+This module serves as a namespace for the various query classes available for interacting with the TikTok API.
+It encapsulates the functionality for handling different types of API queries related to user and playlist data.
+"""
