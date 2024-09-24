5c5c6c979aabbc70b0da36172eb863b837d6083d
Alexandre Teles <1757773+alexandreteles@users.noreply.github.com>
2024-09-24 02:00:04 -0300

    feat: add comment downloading

--- TikTok/Queries/Video.py
+++ TikTok/Queries/Video.py
@@ -37,3 +37,39 @@ class VideoQueries(QueryClass[RequestModel, ResponseModel]):
             params=self._build_params(fields),
             json_data=request.model_dump(by_alias=True, exclude_none=True),
         )
+
+    async def comments(
+        self,
+        video_id: int,
+        fields: list[Video.VideoCommentFields],
+        max_count: int | None = None,
+        cursor: int | None = None,
+    ) -> Video.VideoCommentResponseModel:
+        """
+        Retrieves comments for a specific video.
+
+        Parameters:
+            video_id (int): The ID of the video for which to retrieve comments.
+            fields (list[Video.VideoCommentFields]): A list of fields to include in the response.
+            max_count (int | None): The maximum number of comments to retrieve. Defaults to None.
+            cursor (int | None): The cursor for pagination. Defaults to None.
+
+        Returns:
+            Video.VideoCommentResponseModel: The response model containing the video comments.
+
+        Raises:
+            ValueError: If video_id is not a positive integer.
+        """
+        return await self._fetch_data(
+            url=self.query.endpoints.VideoCommentsURL,
+            request_model_class=Video.VideoCommentRequestModel,
+            response_model_class=Video.VideoCommentResponseModel,
+            params=self._build_params(fields),
+            json_data=self._build_json_data(
+                {
+                    "video_id": video_id,
+                    "max_count": max_count,
+                    "cursor": cursor,
+                }
+            ),
+        )
